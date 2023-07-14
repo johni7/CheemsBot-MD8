207c7240113fb7b97b266ed208a7a17bb97b7058
johni7 <139517801+johni7@users.noreply.github.com>
2023-07-14 23:39:57 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,4 @@
-const chalk = require("chalk")
-const fs = require("fs")
-
+const chalk = require("chalk")const fs = require("fs")
 //aumto presence update
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
@@ -21,27 +19,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['923400470983'] //ur owner number
+global.ownernomer = "923400470983" //ur owner number2
+global.ownername = "🦄Raju" //ur owner name
+global.ytname = "YT: DSPBOSSYT" //ur yt chanel name
+global.socialm = "https://github.com/johni7/CheemsBot-MD8: Raju" //ur github or insta name
+global.location = "Pakistan , Punjab , Faisalabad" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Raju"
+global.ownernumber = '923400470983'
+global.ownername = '🦄Raju'
+global.ownerNumber = ["923400470983@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@DSPBOSSYT"
+global.websitex = "https://youtu.be/dspbossyt"
+global.wagc = "https://chat.whatsapp.com/Eeo2DAxu5ec4wrIoSxZhs5"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botscript = 'https://github.com/johni7/CheemsBot-MD8' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄Raju\n\n+923400470983"
+global.creator = "923400470983@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
